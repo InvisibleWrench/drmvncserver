5b5880e4ea841723b3931c05ddb6e93159a97d68
Morten Boye Mortensen <morten@invisiblewrench.com>
2024-06-07 13:13:08 +0200

    logging fix

--- src/logging.h
+++ src/logging.h
@@ -1,23 +1,28 @@
 #pragma once
 
-#define error_print(...)
-do
-{
-    fprintf(stderr, __VA_ARGS__);
-} while (0)
+#include <stdio.h> // Include necessary header for fprintf
 
-#define info_print(...)
-    do
-{
-    fprintf(stderr, __VA_ARGS__);
-}
-while (0)
+// Define error_print macro
+#define error_print(...) \
+    do \
+    { \
+        fprintf(stderr, __VA_ARGS__); \
+    } while (0)
 
-    extern int verbose;
+// Define info_print macro
+#define info_print(...) \
+    do \
+    { \
+        fprintf(stderr, __VA_ARGS__); \
+    } while (0)
 
-#define debug_print(...)
-do
-{
-    if (verbose)
-        fprintf(stderr, __VA_ARGS__);
-} while (0)
+// Declare extern variable 'verbose'
+extern int verbose;
+
+// Define debug_print macro
+#define debug_print(...) \
+    do \
+    { \
+        if (verbose) \
+            fprintf(stderr, __VA_ARGS__); \
+    } while (0)
